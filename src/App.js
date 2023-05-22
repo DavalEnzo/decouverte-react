07f6ca56cf92696d4eb90365d9877c582ac5aa3d
DavalEnzo <davalenzo@gmail.com>
2023-05-22 11:53:30 +0200

    1er commit leçon reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logoNode from './assets/img/logo_node.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logoNode} className='App-logo' alt='logo' />
+                <p className='waveTextAnimated'>
+                    <span>T</span>
+                    <span>e</span>
+                    <span>s</span>
+                    <span>t</span>
+                </p>
+                <a
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
